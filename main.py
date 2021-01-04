115b695139bf8998f332868c6750538ebe2a85b3
kolaider <kolja87@tut.by>
2021-01-04 17:57:35 +0300

    creating of the playing field

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pygame
-
+SIZE_BLOCK = 20
 FRAME_COLOR = (0, 255, 204)
+WHITE = (255, 255, 255)
+BLUE = (204, 255, 255)
 size = [600, 800]
+COUNT_BLOCKS = 25
+MARGIN = 1
 
 screen = pygame.display.set_mode(size)      #creating window
 pygame.display.set_caption('Snake')         #setting caption
@@ -13,4 +17,14 @@ while True:
             pygame.quit()
 
     screen.fill(FRAME_COLOR)
+
+    for row in range(COUNT_BLOCKS):
+        for column in range(COUNT_BLOCKS):
+            if (row + column)%2==0:
+                color = BLUE
+            else:
+                color = WHITE
+            pygame.draw.rect(screen, color, (10+column*SIZE_BLOCK + MARGIN*(column+1),
+                                             20 + row * SIZE_BLOCK + MARGIN*(row + 1), SIZE_BLOCK, SIZE_BLOCK))
+
     pygame.display.flip()
