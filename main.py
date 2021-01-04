54e321409f22be391c3f86a6e8c5cfdd032cc3f6
kolaider <kolja87@tut.by>
2021-01-04 17:34:38 +0300

    added event of closing the window

--- main.py
+++ main.py
@@ -2,4 +2,11 @@ import pygame
 
 size = [600, 800]
 
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode(size)      #creating window
+pygame.display.set_caption('Snake')         #setting caption
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
