8d6d60998b4f39cdc88e11b57d55623811c56ac3
kolaider <kolja87@tut.by>
2021-01-04 19:31:54 +0300

    fixing apple in random cell

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pygame
 import sys
+import random
 SIZE_BLOCK = 20
 FRAME_COLOR = (0, 255, 204)
 WHITE = (255, 255, 255)
 BLUE = (204, 255, 255)
+RED = (224, 0, 0)
 HEADER_COLOR = (0, 204, 153)
 SNAKE_COLOR = (0, 102, 0)
 COUNT_BLOCKS = 25
@@ -27,12 +29,26 @@ class SnakeBlock:
     def is_inside(self):
         return 0 <= self.x < COUNT_BLOCKS and 0 <= self.y < COUNT_BLOCKS
 
+    def __eq__(self, other):
+        return isinstance(other, SnakeBlock) and self.x == other.x and self.y == other.y
+
+def get_random_block():
+    x = random.randint(0, COUNT_BLOCKS-1)
+    y = random.randint(0, COUNT_BLOCKS-1)
+    empty_block = SnakeBlock(x, y)
+    while empty_block in snake_blocks:
+        empty_block.x = random.randint(0, COUNT_BLOCKS-1)
+        empty_block.y = random.randint(0, COUNT_BLOCKS-1)
+    return empty_block
+
+
 def draw_block(color, row, column):
     pygame.draw.rect(screen, color, (SIZE_BLOCK + column * SIZE_BLOCK + MARGIN * (column + 1),
                                      HEADER_MARGIN + SIZE_BLOCK + row * SIZE_BLOCK + MARGIN * (row + 1), SIZE_BLOCK,
                                      SIZE_BLOCK))
 
 snake_blocks = [SnakeBlock(10,9), SnakeBlock(10,10), SnakeBlock(10,11)]
+apple = get_random_block()
 
 d_row = 0
 d_col = 1
@@ -76,9 +92,14 @@ while True:
         pygame.quit()
         sys.exit()
 
+    draw_block(RED, apple.x, apple.y)
 
     for block in snake_blocks:
         draw_block(SNAKE_COLOR, block.x, block.y)
+
+
+    if apple == head:
+        apple = get_random_block()
 
 
     new_head = SnakeBlock(head.x + d_row, head.y + d_col)
