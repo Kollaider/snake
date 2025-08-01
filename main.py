62c20a047a00644ea14250c42725172057722db2
Kollaider <nikolai.strom87@gmail.com>
2025-08-01 14:19:48 +0300

    update libs

--- main.py
+++ main.py
@@ -141,12 +141,12 @@ def start_the_game():
 
 
 
-menu = pygame_menu.Menu(300, 400, 'Snake menu',
+menu = pygame_menu.Menu('Snake menu', 300, 400,
                        theme=pygame_menu.themes.THEME_BLUE)
 
-menu.add_text_input('Name :', default='Player 1')
+menu.add.text_input('Name :', default='Player 1')  # Note the dot after add
 # menu.add_selector('Difficulty :', [('Hard', 1), ('Easy', 2)], onchange=set_difficulty)
-menu.add_button('Play', start_the_game)
-menu.add_button('Quit', pygame_menu.events.EXIT)
+menu.add.button('Play', start_the_game)
+menu.add.button('Quit', pygame_menu.events.EXIT)
 
 menu.mainloop(screen)
